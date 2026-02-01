83e2c64d413e861a207c4feec0fe17da75220b3f
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-02-01 14:26:49 +0545

    Added:invoices Url

--- config/urls.py
+++ config/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('core/', include('apps.core.urls')),
+    path('api/', include('apps.invoices.urls'))
 ]
