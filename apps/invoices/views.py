132d51886994819b177ef1e3fbcde10f97fc5107
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-02-02 17:57:34 +0545

    Added:Authenticateed and owneroradmin

--- apps/invoices/views.py
+++ apps/invoices/views.py
@@ -1,9 +1,12 @@
 from rest_framework.viewsets import ModelViewSet
 from .models import Invoice
 from .serializers import InvoiceSerializer
+from rest_framework.permissions import IsAuthenticated
+from apps.core.permissions import IsOwnerOrAdmin
 
 class InvoiceViewSet(ModelViewSet):
     serializer_class = InvoiceSerializer
+    permission_classes = [IsAuthenticated, IsOwnerOrAdmin]
 
     def get_queryset(self):
         return Invoice.objects.filter(
@@ -12,3 +15,5 @@ class InvoiceViewSet(ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(organization=self.request.organization)
+
+
