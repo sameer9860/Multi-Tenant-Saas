c4487df298d57a5819358e64b3a7d586aa9aa9b3
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-02-04 19:01:14 +0545

    Fixed:Usage serializers

--- apps/billing/views.py
+++ apps/billing/views.py
@@ -4,8 +4,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework import status
-from rest_framework import serializers
+from .serializers import UsageSerializer
 class UpgradePlanView(APIView):
     permission_classes = [IsAuthenticated]
 
