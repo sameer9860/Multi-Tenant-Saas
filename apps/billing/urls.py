9b23bfd4734b0a978ac051e1a6fc230a8dc33d77
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-02-05 18:46:28 +0545

    Added:upgrade url

--- apps/billing/urls.py
+++ apps/billing/urls.py
@@ -5,5 +5,6 @@ from .views import UsageDashboardAPIView, UsageDashboardView
 
 urlpatterns = [
     path("usage/", UsageDashboardAPIView.as_view(), name="usage-dashboard"),
+    path("upgrade/", UsageDashboardView.as_view(), name="usage-dashboard-upgrade"),
     path("usage/ui/", UsageDashboardView.as_view(), name="usage-dashboard-ui"),
 ]
