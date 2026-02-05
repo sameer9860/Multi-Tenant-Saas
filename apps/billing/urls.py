aaec81c9d8ad3b9420d6955ca477c039a01d7f68
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-02-05 18:52:50 +0545

    Fixed Upgrade plan view

--- apps/billing/urls.py
+++ apps/billing/urls.py
@@ -1,10 +1,10 @@
 # apps/billing/urls.py
 
 from django.urls import path
-from .views import UsageDashboardAPIView, UsageDashboardView
+from .views import UsageDashboardAPIView, UsageDashboardView,UpgradePlanAPIView
 
 urlpatterns = [
-    path("usage/", UsageDashboardAPIView.as_view(), name="usage-dashboard"),
-    path("upgrade/", UsageDashboardView.as_view(), name="usage-dashboard-upgrade"),
+    path("usage/", UsageDashboardAPIView.as_view(), name="usage-dashboard"),    
     path("usage/ui/", UsageDashboardView.as_view(), name="usage-dashboard-ui"),
+    path("upgrade/", UpgradePlanAPIView.as_view(), name="upgrade-plan"),
 ]
