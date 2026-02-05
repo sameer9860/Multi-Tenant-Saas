bd93a8b05185da1a8ce1b7ecb8cf22ce4a275788
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-02-05 18:58:00 +0545

    Added:Usage and payment transaction to admin

--- apps/billing/admin.py
+++ apps/billing/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Subscription
+from .models import Subscription, Usage ,PaymentTransaction
 
 # Register your models here.
 admin.site.register(Subscription)
+admin.site.register(Usage)
+admin.site.register(PaymentTransaction)
