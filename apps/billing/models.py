824759b27fbaf00dbdd6103dc91c2e5ff94c6e44
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-02-06 19:52:11 +0545

    Added:Payment model

--- apps/billing/models.py
+++ apps/billing/models.py
@@ -35,6 +35,22 @@ class Usage(models.Model):
     invoices_created = models.PositiveIntegerField(default=0)
     updated_at = models.DateTimeField(auto_now=True)
     
+class Payment(models.Model):
+    STATUS_CHOICES = (
+        ('PENDING', 'Pending'),
+        ('SUCCESS', 'Success'),
+        ('FAILED', 'Failed'),
+    )
+
+    organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
+    amount = models.PositiveIntegerField()
+    plan = models.CharField(max_length=20)
+    transaction_id = models.CharField(max_length=100, blank=True, null=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='PENDING')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.organization.name} - {self.plan} - {self.status}"
     
 class PaymentTransaction(models.Model):
     PAYMENT_PROVIDERS = (
