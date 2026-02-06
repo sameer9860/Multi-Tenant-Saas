704912fe4167a032eb6605b3f34cea6cad925fd0
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-02-06 20:29:31 +0545

    Added usage dashboard url

--- apps/analytics/urls.py
+++ apps/analytics/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import UsageView
+from .views import UsageView,usage_dashboard
+
 
 urlpatterns = [
     path("usage/", UsageView.as_view(), name="usage"),
+    path("usage/dashboard/", usage_dashboard, name="usage-dashboard"),
 ]
