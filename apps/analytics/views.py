793a41ae8eaf657660ae0d540835bfb9b96cf85d
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-02-06 20:23:09 +0545

    Added:Usageview analytics

--- apps/analytics/views.py
+++ apps/analytics/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+class UsageView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        org = request.organization
+        plan = org.subscription.plan
+
+        limits = {
+            "FREE": 10,
+            "PRO": 1000,
+            "BUSINESS": 10000,
+        }
+
+        used = org.invoices.count()
+
+        return Response({
+            "plan": plan,
+            "used": used,
+            "limit": limits.get(plan, 0),
+            "remaining": limits.get(plan, 0) - used
+        })
