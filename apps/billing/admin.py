97a77c3ddeb25e582b0d61556219e6f4bdb5fb63
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-02-06 19:55:47 +0545

    Added:PAyement to the admin

--- apps/billing/admin.py
+++ apps/billing/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Subscription, Usage ,PaymentTransaction
+from .models import Subscription, Usage ,PaymentTransaction,Payment
 
 # Register your models here.
 admin.site.register(Subscription)
 admin.site.register(Usage)
 admin.site.register(PaymentTransaction)
+admin.site.register(Payment)
