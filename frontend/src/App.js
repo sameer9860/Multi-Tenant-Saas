7c841391c5be4312173328be476df0ae2fde8bdb
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-02-08 18:22:53 +0545

    Added:Packages

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Pricing from './pages/Pricing';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/pricing" />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/login" element={<div className="p-10">Login page (to be implemented)</div>} />
+        <Route path="/dashboard" element={<div className="p-10">Dashboard (to be implemented)</div>} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
