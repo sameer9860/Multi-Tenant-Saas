28446ea6035ce0fd2c7f40de040dd3dd8e920e04
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-02-11 18:06:38 +0545

    Created:Lead Module

--- crm/models.py
+++ crm/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from organizations.models import Organization
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Lead(models.Model):
+
+    STATUS_CHOICES = [
+        ("NEW", "New"),
+        ("CONTACTED", "Contacted"),
+        ("CONVERTED", "Converted"),
+        ("LOST", "Lost"),
+    ]
+
+    organization = models.ForeignKey(
+        Organization,
+        on_delete=models.CASCADE,
+        related_name="leads"
+    )
+
+    name = models.CharField(max_length=255)
+    email = models.EmailField(blank=True, null=True)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+
+    source = models.CharField(max_length=100, blank=True, null=True)
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default="NEW"
+    )
+
+    assigned_to = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
