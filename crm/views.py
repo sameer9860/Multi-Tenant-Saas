264fdc4bf71958de1685f8f5f1159d2343948e93
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-02-11 18:10:26 +0545

    Created:views for crm

--- crm/views.py
+++ crm/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Lead, Client
+from .serializers import LeadSerializer, ClientSerializer
 
-# Create your views here.
+
+class LeadViewSet(viewsets.ModelViewSet):
+    serializer_class = LeadSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Lead.objects.filter(
+            organization=self.request.organization
+        )
+
+    def perform_create(self, serializer):
+        serializer.save(
+            organization=self.request.organization
+        )
+
+
+class ClientViewSet(viewsets.ModelViewSet):
+    serializer_class = ClientSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Client.objects.filter(
+            organization=self.request.organization
+        )
+
+    def perform_create(self, serializer):
+        serializer.save(
+            organization=self.request.organization
+        )
