16ab3dee42cee670d84f3b2ce7fde8cf1a036279
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-02-11 18:08:59 +0545

    Fixed:Models

--- crm/models.py
+++ crm/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-from organizations.models import Organization
 from django.contrib.auth import get_user_model
+from apps.core.models import Organization   
 
 User = get_user_model()
 
