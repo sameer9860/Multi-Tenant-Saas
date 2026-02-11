70d35a96fc175e3a839017664ed7b0e7611abaca
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-02-11 18:07:16 +0545

    Created:Client Model

--- crm/models.py
+++ crm/models.py
@@ -41,3 +41,25 @@ class Lead(models.Model):
 
     def __str__(self):
         return self.name
+
+
+
+class Client(models.Model):
+
+    organization = models.ForeignKey(
+        Organization,
+        on_delete=models.CASCADE,
+        related_name="clients"
+    )
+
+    name = models.CharField(max_length=255)
+    email = models.EmailField(blank=True, null=True)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    company = models.CharField(max_length=255, blank=True, null=True)
+    notes = models.TextField(blank=True, null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
