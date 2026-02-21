7c45d13cf7613c2190b6a5cbc9febf2cd0d86d0d
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-02-21 18:10:19 +0545

    Updated Serializer

--- apps/accounts/serializers.py
+++ apps/accounts/serializers.py
@@ -2,15 +2,19 @@ from rest_framework import serializers
 from .models import User, OrganizationMember
 
 class UserSerializer(serializers.ModelSerializer):
+    role_name = serializers.CharField(source='role.name', read_only=True)
+    
     class Meta:
         model = User
-        fields = ['id', 'email', 'full_name', 'role']
+        fields = ['id', 'email', 'full_name', 'role', 'role_name']
 
 class OrganizationMemberSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(source='user.email', read_only=True)
     full_name = serializers.CharField(source='user.full_name', read_only=True)
     phone = serializers.CharField(source='user.phone', read_only=True)
     
+    role_name = serializers.CharField(source='role.name', read_only=True)
+    
     class Meta:
         model = OrganizationMember
-        fields = ['id', 'email', 'full_name', 'phone', 'role', 'created_at']
+        fields = ['id', 'email', 'full_name', 'phone', 'role', 'role_name', 'created_at']
