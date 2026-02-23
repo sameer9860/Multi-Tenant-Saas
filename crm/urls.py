76fa45399dd74d50d7890c4f60d6aed9ba6b77d2
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-02-23 08:34:54 +0545

    Updated url with expences

--- crm/urls.py
+++ crm/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import LeadViewSet, ClientViewSet, DashboardView, LeadActivityViewSet
+from .views import LeadViewSet, ClientViewSet, DashboardView, LeadActivityViewSet, ExpenseViewSet
 
 router = DefaultRouter()
 router.register(r"leads", LeadViewSet, basename="lead")
 router.register(r"clients", ClientViewSet, basename="client")
 router.register(r"activities", LeadActivityViewSet, basename="activity")
+router.register(r"expenses", ExpenseViewSet, basename="expense")
 
 urlpatterns = [
     path("dashboard/", DashboardView.as_view(), name="dashboard"),
