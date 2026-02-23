0725a54a3104bbdcc0f8a54f5b10cd8c76d2608c
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-02-23 08:33:58 +0545

    Added:Expences Model

--- crm/models.py
+++ crm/models.py
@@ -91,3 +91,17 @@ class LeadActivity(models.Model):
 
     def __str__(self):
         return f"{self.lead.name} - {self.action}"
+
+class Expense(models.Model):
+    organization = models.ForeignKey(
+        Organization,
+        on_delete=models.CASCADE,
+        related_name="expenses"
+    )
+    title = models.CharField(max_length=255)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.amount}"
