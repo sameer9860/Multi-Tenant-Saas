697029a824d9d8abc06848b6e6c09a3be998a13e
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-02-23 08:37:24 +0545

    Updated admin with expence

--- crm/admin.py
+++ crm/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
-from .models import Lead, Client,LeadActivity
-
-
+from .models import Lead, Client, LeadActivity, Expense
 
 # Register your models here.
 admin.site.register(Lead)
 admin.site.register(Client)
 admin.site.register(LeadActivity)
+admin.site.register(Expense)
     
