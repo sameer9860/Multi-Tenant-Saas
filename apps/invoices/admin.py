469b2898d6d9eb12e58c0534d9bab4aa6ae52ac2
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-02-24 08:01:04 +0545

    Added to admin partial payment

--- apps/invoices/admin.py
+++ apps/invoices/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Invoice, InvoiceItem, Customer, InvoicePayment
+from .models import Invoice, InvoiceItem, Customer, Payment
 # Register your models here.
 admin.site.register(Invoice)
 admin.site.register(InvoiceItem)
 admin.site.register(Customer)
-admin.site.register(InvoicePayment)
+admin.site.register(Payment)
