7090a2bcb0e89f4fd52b543aea0712e373efec7c
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-02-24 07:59:31 +0545

    Updated urls with partial payment view

--- apps/invoices/urls.py
+++ apps/invoices/urls.py
@@ -1,11 +1,11 @@
 from rest_framework.routers import DefaultRouter
-from .views import InvoiceViewSet, CustomerViewSet, InvoiceItemViewSet, InvoicePaymentViewSet
+from .views import InvoiceViewSet, CustomerViewSet, InvoiceItemViewSet, PaymentViewSet
 
 router = DefaultRouter()
 router.register(r'customers', CustomerViewSet, basename='customer')
 router.register(r'invoices', InvoiceViewSet, basename='invoice')
 router.register(r'invoice-items', InvoiceItemViewSet, basename='invoice-item')
-router.register(r'invoice-payments', InvoicePaymentViewSet, basename='invoice-payment')
+router.register(r'payments', PaymentViewSet, basename='payment')
 
 urlpatterns = router.urls
 
