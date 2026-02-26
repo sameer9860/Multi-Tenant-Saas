fd44b146b9e40a3fa5c9a7236080fe78a4305921
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-02-26 19:25:35 +0545

    Updated with invoices

--- config/urls.py
+++ config/urls.py
@@ -36,6 +36,12 @@ urlpatterns = [
     # for payment-related endpoints
 ]
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
 from django.urls import re_path
 
