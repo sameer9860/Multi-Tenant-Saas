5463056362bf3744cdeb335b614e3b2bb0f24909
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-02-26 19:21:44 +0545

    Updated org with logo a

--- apps/core/models.py
+++ apps/core/models.py
@@ -16,6 +16,8 @@ class Organization(models.Model):
         ],
         default='FREE'
     )
+    vat_number = models.CharField(max_length=50, blank=True, null=True)
+    logo = models.ImageField(upload_to='org_logos/', blank=True, null=True)
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
