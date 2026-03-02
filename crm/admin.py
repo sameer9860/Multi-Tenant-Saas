6bce3c7863ff325271de5c8ae564e36f902f1c44
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-03-02 14:43:37 +0545

    Added note remainder and interaction

--- crm/admin.py
+++ crm/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from .models import Lead, Client, LeadActivity, Expense
+from .models import Lead, Client, LeadActivity, Expense, Note, Interaction, Reminder
 
 # Register your models here.
 admin.site.register(Lead)
 admin.site.register(Client)
 admin.site.register(LeadActivity)
 admin.site.register(Expense)
-    
+admin.site.register(Note)
+admin.site.register(Interaction)
+admin.site.register(Reminder)    
