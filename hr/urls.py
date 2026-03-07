a789c78bdcf733fbc204d4f49aca209f2e0abe1d
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-03-07 08:23:14 +0545

    Added attendence route

--- hr/urls.py
+++ hr/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import EmployeeViewSet, DepartmentViewSet, DesignationViewSet
+from .views import EmployeeViewSet, DepartmentViewSet, DesignationViewSet, AttendanceViewSet
 
 router = DefaultRouter()
 router.register(r'departments', DepartmentViewSet, basename='department')
 router.register(r'designations', DesignationViewSet, basename='designation')
 router.register(r'employees', EmployeeViewSet, basename='employee')
+router.register(r'attendance', AttendanceViewSet, basename='attendance')
+
 
 urlpatterns = [
     path('', include(router.urls)),
