020708538d7da89f8aabecc1ee3794941ba4639a
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-03-09 18:22:48 +0545

    Added leave route

--- hr/urls.py
+++ hr/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import EmployeeViewSet, DepartmentViewSet, DesignationViewSet, AttendanceViewSet
+from .views import EmployeeViewSet, DepartmentViewSet, DesignationViewSet, AttendanceViewSet, LeaveRequestViewSet
 
 router = DefaultRouter()
 router.register(r'departments', DepartmentViewSet, basename='department')
 router.register(r'designations', DesignationViewSet, basename='designation')
 router.register(r'employees', EmployeeViewSet, basename='employee')
 router.register(r'attendance', AttendanceViewSet, basename='attendance')
+router.register(r'leave-requests', LeaveRequestViewSet, basename='leave-request')
 
 
 urlpatterns = [
