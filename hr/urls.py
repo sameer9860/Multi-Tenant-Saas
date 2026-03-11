08933a149ad42a18eaaeab6965c57296208e280c
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-03-11 18:57:06 +0545

    Added form urls

--- hr/urls.py
+++ hr/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import EmployeeViewSet, DepartmentViewSet, DesignationViewSet, AttendanceViewSet, LeaveRequestViewSet, PayrollViewSet
+from .views import (
+    EmployeeViewSet, DepartmentViewSet, DesignationViewSet, 
+    AttendanceViewSet, LeaveRequestViewSet, PayrollViewSet, SalaryAdvanceViewSet
+)
 
 router = DefaultRouter()
 router.register(r'departments', DepartmentViewSet, basename='department')
@@ -9,6 +12,7 @@ router.register(r'employees', EmployeeViewSet, basename='employee')
 router.register(r'attendance', AttendanceViewSet, basename='attendance')
 router.register(r'leave-requests', LeaveRequestViewSet, basename='leave-request')
 router.register(r'payrolls', PayrollViewSet, basename='payroll')
+router.register(r'salary-advances', SalaryAdvanceViewSet, basename='salary-advance')
 
 
 urlpatterns = [
