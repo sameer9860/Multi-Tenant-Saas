870587de9333fab51a1913a7bfb550b993da522f
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-03-14 17:30:13 +0545

    Added appointment url

--- config/urls.py
+++ config/urls.py
@@ -34,6 +34,7 @@ urlpatterns = [
     path('analytics/', include('apps.analytics.urls')),  # new line for analytics endpoints
     path("api/crm/", include("crm.urls")),
     path("api/hr/", include("hr.urls")),
+    path("api/appointments/", include("appointments.urls")),
     # for payment-related endpoints
 ]
 
