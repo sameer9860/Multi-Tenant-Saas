efcf9316d29b72557d2e259d4ce7047819227bf7
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-03-16 20:10:01 +0545

    Added route staff

--- appointments/urls.py
+++ appointments/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ServiceViewSet
+from .views import ServiceViewSet, StaffViewSet
 
 router = DefaultRouter()
 router.register(r'services', ServiceViewSet, basename='service')
+router.register(r'staff', StaffViewSet, basename='staff')
 
 urlpatterns = [
     path('', include(router.urls)),
