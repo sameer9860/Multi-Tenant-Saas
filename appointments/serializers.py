ba0df1cbd01632b02066f76df0283638c4de4b6a
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-03-16 20:09:39 +0545

    Created staff

--- appointments/serializers.py
+++ appointments/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Service
+from .models import Service, Staff
 
 class ServiceSerializer(serializers.ModelSerializer):
     class Meta:
@@ -9,6 +9,17 @@ class ServiceSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         # Automatically assign the organization from the user's profile
+        user = self.context['request'].user
+        validated_data['organization'] = user.organization
+        return super().create(validated_data)
+
+class StaffSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Staff
+        fields = '__all__'
+        read_only_fields = ('organization', 'created_at')
+
+    def create(self, validated_data):
         user = self.context['request'].user
         validated_data['organization'] = user.organization
         return super().create(validated_data)
