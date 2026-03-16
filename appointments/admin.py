1cf1e5d347b786098c3c7966062d28dbcfc60e06
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-03-16 20:13:27 +0545

    Updated admin with staff

--- appointments/admin.py
+++ appointments/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Service
+from .models import Service, Staff
 
 # Register your models here.
 admin.site.register(Service)
+admin.site.register(Staff)
