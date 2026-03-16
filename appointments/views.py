78035ac5a87ef69540ca80422f44080389419f34
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-03-16 20:09:13 +0545

    Updated with staff viewset

--- appointments/views.py
+++ appointments/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets, permissions
-from .models import Service
-from .serializers import ServiceSerializer
+from .models import Service, Staff
+from .serializers import ServiceSerializer, StaffSerializer
 
 class ServiceViewSet(viewsets.ModelViewSet):
     serializer_class = ServiceSerializer
@@ -12,4 +12,14 @@ class ServiceViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         # Ensure organization is set during creation (as a backup to serializer logic)
+        serializer.save(organization=self.request.user.organization)
+
+class StaffViewSet(viewsets.ModelViewSet):
+    serializer_class = StaffSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Staff.objects.filter(organization=self.request.user.organization)
+
+    def perform_create(self, serializer):
         serializer.save(organization=self.request.user.organization)
