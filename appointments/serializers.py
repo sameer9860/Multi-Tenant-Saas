c7af312fe6987a10a4a5fbcd4835eee155f70ef6
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-03-17 07:31:10 +0545

    Added serializers for availability

--- appointments/serializers.py
+++ appointments/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Service, Staff
+from .models import Service, Staff, StaffAvailability
 
 class ServiceSerializer(serializers.ModelSerializer):
     class Meta:
@@ -23,3 +23,11 @@ class StaffSerializer(serializers.ModelSerializer):
         user = self.context['request'].user
         validated_data['organization'] = user.organization
         return super().create(validated_data)
+
+class StaffAvailabilitySerializer(serializers.ModelSerializer):
+    day_of_week_display = serializers.CharField(source='get_day_of_week_display', read_only=True)
+    staff_name = serializers.CharField(source='staff.name', read_only=True)
+
+    class Meta:
+        model = StaffAvailability
+        fields = '__all__'
