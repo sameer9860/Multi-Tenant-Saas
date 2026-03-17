ad135105882e201910e7a3dc0dcd90b50353c3f7
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-03-17 07:32:15 +0545

    Added availability url

--- appointments/urls.py
+++ appointments/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ServiceViewSet, StaffViewSet
+from .views import ServiceViewSet, StaffViewSet, StaffAvailabilityViewSet
 
 router = DefaultRouter()
 router.register(r'services', ServiceViewSet, basename='service')
 router.register(r'staff', StaffViewSet, basename='staff')
+router.register(r'staff-availability', StaffAvailabilityViewSet, basename='staff-availability')
 
 urlpatterns = [
     path('', include(router.urls)),
