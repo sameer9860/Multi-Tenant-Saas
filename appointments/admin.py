1ee31bb3246da2908fbc2d7b6ab3a3faca1c5c75
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-03-21 20:12:45 +0545

    Added to admin

--- appointments/admin.py
+++ appointments/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Service, Staff, Appointment
+from .models import Service, Staff, Appointment, AppointmentReminder
 
 # Register your models here.
 admin.site.register(Service)
 admin.site.register(Staff)
 admin.site.register(Appointment)
+admin.site.register(AppointmentReminder)
